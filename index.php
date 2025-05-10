719e6753b6ba504a104dab86788746c4404d4e85
Hotaru-git3 <zidanesolahudin123@gmail.com>
2025-05-10 16:50:08 +0700

    menambah file php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <body>
+        <h2>php_check_syntax</h2>
+    </body>
+</body>
+</html>
